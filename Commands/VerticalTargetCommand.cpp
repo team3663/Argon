96c4848e01f6cd0ef1f2179583e44034ee9509ef
jwiederspan <jon@wiederspan.org>
2013-04-03 10:05:31 -0700

    Added some comments and removed dead code
    This still needs to be modified to use the center y pixel value for our target, not the center of the image

--- Commands/VerticalTargetCommand.cpp
+++ Commands/VerticalTargetCommand.cpp
@@ -20,18 +20,16 @@ void VerticalTargetCommand::Execute()
 {
 	if (Robot::targetting->Target())
 	{
-		DriverStationLCD::GetInstance()->PrintfLine(DriverStationLCD::kUser_Line5, "Targetting Worked");
-		DriverStationLCD::GetInstance()->UpdateLCD();
+		// get the number of pixels off in y-axis from ideal target
 		double difference = Robot::targetting->GetTargetRect()->center_mass_y_normalized;
 		if (difference > 0.0)
 		{
-			//Robot::pitch->IncreasePitch(difference * 5);
-			IncreasePitchCommand(difference * 5).Run();
+			// make sure this runs in sequence, not parallel
+			IncreasePitchCommand(0.8, difference * 5).Run();
 		}
 		else
 		{
-			//Robot::pitch->DecreasePitch(difference * -5);
-			IncreasePitchCommand(difference * 5).Run();
+			DecreasePitchCommand(-0.8, difference * -5).Run();
 		}
 		
 		if (difference < 0.03 && difference > -0.03)
