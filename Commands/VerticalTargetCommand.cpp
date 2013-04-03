4358f0909499eb8c7e519841d3c9b698c3c69c5f
jwiederspan <jon@wiederspan.org>
2013-04-03 10:12:25 -0700

    Added constants for vertical pixel center of target

--- Commands/VerticalTargetCommand.cpp
+++ Commands/VerticalTargetCommand.cpp
@@ -1,5 +1,9 @@
 #include "VerticalTargetCommand.h"
 
+// This currently only aims for the three point target
+#define TARGET_CENTER  200   // center x pixel of the three point target where 0,0 is top-left of the image
+#define TOLERANCE        2   // number of pixels to allow the target to be off and still accept as centered
+
 VerticalTargetCommand::VerticalTargetCommand()
 {
 	done = false;
@@ -21,7 +25,8 @@ void VerticalTargetCommand::Execute()
 	if (Robot::targetting->Target())
 	{
 		// get the number of pixels off in y-axis from ideal target
-		double difference = Robot::targetting->GetTargetRect()->center_mass_y_normalized;
+		// UPDATE THIS TO USE PIXELS WITH 0,0 AT TOP_LEFT OF IMAGE
+		float difference = Robot::targetting->GetTargetRect()->center_mass_y_normalized;
 		if (difference > 0.0)
 		{
 			// make sure this runs in sequence, not parallel
@@ -37,8 +42,6 @@ void VerticalTargetCommand::Execute()
 	}
 	else
 	{
-		DriverStationLCD::GetInstance()->PrintfLine(DriverStationLCD::kUser_Line5, "Targetting Failed");
-		DriverStationLCD::GetInstance()->UpdateLCD();
 		done = true;
 	}
 }
